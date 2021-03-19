add114e92dfe1a01a097032c34f8521a82dd1229
nesousx <denis+github@nesomail.com>
2021-03-19 10:23:46 +0100

    revamped script

--- i3lock-bing.sh
+++ i3lock-bing.sh
@@ -1,120 +1,38 @@
 #!/usr/bin/env bash
 
-# i3lock-bing
-# March 16 2021, Denis G.
-# Make this script pull Bing Wallpaper of the Day + caption
-# and set it as lockscreen
+#
+# i3lock with Bing Wallpaper of the Day
+# Denis G.
+# March 19, 2021
+# WIP
+# TODO :
+# * Single call to API ;
+# * Do no call API / script it image already exists.
+#
 
-# Shamelessly based on :
-# author: Whizzzkid (me@nishantarora.in)
-# source: https://gist.github.com/b00f/4561c9083622960f6b88bd2fc40a72d2 
+### Variables
+today="`date +%d%m%y`"
 
-# Get Wallpaper
+base_url=http://bing.com
+api_url=https://www.bing.com/HPImageArchive.aspx\?\&format\=js\&idx\=0\&mkt\=en-US\&n\=1
 
-# Base URL.
-bing="http://www.bing.com"
+daily_suffix="`curl -s $api_url | jq -r '.images[]|.url'`"
+copyright="`curl -s $api_url | jq -r '.images[]|.copyright'`"
 
-# API end point.
-api="/HPImageArchive.aspx?"
+lockscreen="/tmp/bing_$today.png"
 
-# Response Format (json|xml).
-format="&format=js"
-
-# For day (0=current; 1=yesterday... so on).
-day="&idx=0"
-
-# Market for image.
-market="&mkt=en-US"
-
-# API Constant (fetch how many).
-const="&n=1"
-
-# Image extension.
-extn=".jpg"
-
-# Size.
-size="1920x1080"
-
-# Collection Path.
-path="/tmp/"
-
-# Make it run just once (useful to run as a cron)
-run_once=false
-while getopts "1" opt; do
-  case $opt in
-    1 )
-      run_once=true
-      ;;
-    \? )
-      echo "Invalid option! usage: \"$0 -1\", to run once and exit"
-      exit 1
-      ;;
-  esac
-done
-
-########################################################################
-#### DO NOT EDIT BELOW THIS LINE #######################################
-########################################################################
-
-# Required Image Uri.
-reqImg=$bing$api$format$day$market$const
-#echo "$reqImg"
-
-# Logging.
-#echo "Pinging Bing API..."
-
-# Fetching API response.
-#apiResp=$(curl -s $reqImg)
-#if [ $? -gt 0 ]; then
-#  echo "Ping failed!"
-#  exit 1
-#fi
-
-# Default image URL in case the required is not available.
-defImgURL=$bing$(echo $apiResp | grep -oP "url\":\"[^\"]*" | cut -d "\"" -f 3)
-
-# Req image url (raw).
-reqImgURL=$bing$(echo $apiResp | grep -oP "urlbase\":\"[^\"]*" | cut -d "\"" -f 3)"_"$size$extn
-
-# Image copyright.
-copyright=$(echo $apiResp | grep -oP "copyright\":\"[^\"]*" | cut -d "\"" -f 3)
-
-wp=$(echo $apiResp | grep -oP "\"wp\":.*," | cut -d "," -f 1)
-
-if [ "$wp" == "\"wp\":false" ] ; then
-  reqImgURL=$defImgURL
+## Download image
+if [ ! -f "$lockscreen" ]; then
+	curl -so /tmp/bing_$today.jpg $base_url$daily_suffix
 fi
 
-# Checking if reqImgURL exists.
-if ! wget --quiet --spider $reqImgURL; then
-  reqImgURL=$defImgURL
+## Convert and add caption
+convert /tmp/bing_$today.jpg -size 600x -background snow2 -fill gray44 -pointsize 24 caption:"$copyright" -gravity SouthEast -composite $lockscreen
+
+## Set lockscreen
+#i3lock -i $lockscreen
+
+## Clean up
+if [ -f "/tmp/bing_$today.jpg" ]; then
+	rm /tmp/bing_$today.jpg
 fi
-
-# Logging.
-#echo "Bing Image of the day: $reqImgURL"
-
-# Getting Image Name.
-imgName=${reqImgURL##*/}
-
-# Create Path Dir.
-mkdir -p $path
-
-# Saving Image to collection.
-curl -s -o $path$imgName $reqImgURL
-
-# Logging.
-#echo "Saving image to $path$imgName"
-
-# Writing copyright.
-#echo "$copyright"  > $path${imgName}".txt"
-#echo "$copyright"
-
-## Now my part !
-
-## Create image for i3lock
-lockscsreen=/tmp/lockscsreen.png
-
-convert $path$imgName -size 600x -background snow2 -fill gray44 -pointsize 24 caption:"$copyright" -gravity SouthEast -composite $lockscsreen
-
-## Run i3lock
-i3lock -i $lockscsreen
