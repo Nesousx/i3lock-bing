0e246b78fa52f51bac950e6703b8ed79f0fc21bf
nesousx <denis+github@nesomail.com>
2021-04-06 10:17:43 +0200

    Testing less call to API + more cleanup of old files

--- i3lock-bing.sh
+++ i3lock-bing.sh
@@ -6,7 +6,7 @@
 # March 19, 2021
 # WIP
 # TODO :
-# * Single call to API ;
+# * Test, test, test !
 #
 # Source for failsafe wallpaper : https://wallhaven.cc/w/13w91g
 
@@ -16,8 +16,12 @@ today="`date +%d%m%y`"
 base_url=http://bing.com
 api_url=https://www.bing.com/HPImageArchive.aspx\?\&format\=js\&idx\=0\&mkt\=en-US\&n\=1
 
-daily_suffix="`curl -s $api_url | jq -r '.images[]|.url'`"
-copyright="`curl -s $api_url | jq -r '.images[]|.copyright'`"
+## Get API file locally to limit number of requests
+api_file="/tmp/bing_api_$today.json"
+curl -so $api_file $api_url
+
+daily_suffix="`cat $api_file | jq -r '.images[]|.url'`"
+copyright="`cat -s $api_file | jq -r '.images[]|.copyright'`"
 
 lockscreen="/tmp/bing_$today.png"
 bkp_lockscreen="$PWD/bkp_lockscreen.png"
@@ -27,9 +31,11 @@ if [ ! -f "$lockscreen" ]; then
 	curl -so /tmp/bing_$today.jpg $base_url$daily_suffix
 elif [ -f "$lockscreen" ]; then
 	i3lock -i $lockscreen
+	#echo "lockscreen from cache"
 	exit 0
 else
 	i3lock -i $bkp_lockscreen
+	#echo "bkp lockscreen"
 	exit 0
 fi
 
@@ -38,8 +44,10 @@ convert /tmp/bing_$today.jpg -size 600x -background snow2 -fill gray44 -pointsiz
 
 ## Set lockscreen
 i3lock -i $lockscreen
+#echo "new lockscreen"
 
 ## Clean up
 if [ -f "/tmp/bing_$today.jpg" ]; then
 	rm /tmp/bing_$today.jpg
 fi
+find /tmp -iname "bing_*" -type f -mtime +1 -delete 2>/dev/null
